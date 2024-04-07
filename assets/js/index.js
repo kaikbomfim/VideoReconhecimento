655e77676487b635f34c6298ab692ecf6f489714
Kaik Bomfim <kaikbomfim@gmail.com>
2024-04-07 20:41:06 -0300

    reconhecimento facial

--- assets/js/index.js
+++ assets/js/index.js
@@ -39,7 +39,7 @@ const loadLabels = () => {
     return Promise.all(labels.map(async label => {
         const descriptions = []
         for (let i = 1; i < 3; i++){
-           const img = await faceapi.fetchImage(`/assets/lib/face-api/labels/${label}/${i}.jpg`)
+           const img = await faceapi.fetchImage(`./assets/lib/face-api/labels/${label}/${i}.jpg`)
            const detections = await faceapi
             .detectSingleFace(img)
             .withFaceLandmarks()
